fbd8b459abafb22c115d27e42e0a42034dc9346d
Srikanth Dannarapu <srikant.dannarapu@gmail.com>
2021-08-05 13:29:55 +0530

    1st commit

---
+++ secure-server/src/main/java/com/sriaknth/mutualauth/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sriaknth.mutualauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+				.x509()
+					.subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+					.userDetailsService(userDetailsService());
+	}
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return (username -> {
+        	//codependent is the CN (client name) in generated cert
+        	if (username.equals("codependent-client") || username.equals("codependent-client2")) {
+                return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+            }else{
+            	return null;
+            }
+        });
+    }
+	
+}
---
+++ secure-server/src/main/java/com/sriaknth/mutualauth/web/HomeRestController.java
@@ -0,0 +1,16 @@
+package com.sriaknth.mutualauth.web;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+
+	@GetMapping("/")
+	public String home(Principal principal){
+		return String.format("Hello %s!", principal.getName());
+	}
+	
+}
